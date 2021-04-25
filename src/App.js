b09650a93889cd915afc54f5288fea47c91e3c22
Daniil Chistyakov <4i.danila@gmail.com>
2021-04-25 12:35:53 +0300

    Realised live updating currency data via WebSocket.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useMemo } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const currencies = useMemo(() => ['BTC', 'ETH'], []);
+  const actions = useMemo(() => ['buy', 'sell'], []);
+
+  const [data, setData] = useState(currencies.map(a => ({
+    currency: a,
+    ...Object.fromEntries(actions.map(a => [a, 'Загрузка...'])),
+  })));
+
+  useEffect(() => {
+    currencies.map((currency, index) => {
+      const socket = new WebSocket('wss://api.exchange.bitcoin.com/api/2/ws');
+
+      socket.addEventListener('open', () => {
+        socket.send(JSON.stringify({
+          method: 'subscribeTrades',
+          params: {
+            symbol: currency + 'USD',
+            limit: 1,
+          },
+        }));
+      });
+
+      socket.addEventListener('message', e => {
+        const { params } = JSON.parse(e.data);
+        if (params) {
+          const action = actions.find(a => a === params.data[0].side);
+          if (action) {
+            setData(data => data.map((a, i) => i === index
+              ? { ...a, [action]: params.data[0].price }
+              : a
+            ));
+          }
+        }
+      });
+    });
+  }, [currencies, actions]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          <tr>
+            <th>Криптовалюта</th>
+            <th>Покупка</th>
+            <th>Продажа</th>
+          </tr>
+          {data.map(n => (
+            <tr key={n.currency}>
+              <td>{n.currency}</td>
+              <td>{n.buy} $</td>
+              <td>{n.sell} $</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
